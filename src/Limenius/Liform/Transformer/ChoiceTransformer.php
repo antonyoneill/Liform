22232b5a16fe16238f9ec9d15b6bb6f721ae3323
Antony O'Neill <antony@mention-me.com>
2017-12-01 11:27:03 +0000

    expect array not object
    Signed-off-by: Antony O'Neill <antony@mention-me.com>

--- src/Limenius/Liform/Transformer/ChoiceTransformer.php
+++ src/Limenius/Liform/Transformer/ChoiceTransformer.php
@@ -45,7 +45,7 @@ class ChoiceTransformer extends AbstractTransformer
                     'minItems' => $this->isRequired($form) ? 1 : 0,
                 ],
                 'uniqueItems' => true,
-                'type' => 'object',
+                'type' => 'array',
             ];
 
             if (count($choices)) {
